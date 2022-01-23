ef59eb878ed0992463e704b51f4111f87cbf2cf6
Evgeniy Vasiyarov <evgeniy.vasiyarov@icloud.com>
2022-01-23 16:36:22 +0400

    practical task 3.8

---
+++ PracticalTask3.8/03_ObjectsAndMethods/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        basket.add("Молоко", 40);
+        basket.add("Хлеб", 35, 2);
+        basket.add("Макровь", 60, 500.0);
+        basket.add("Картофель", 70, 2, 2000.0);
+        basket.print("Корзина:");
+    }
+}
